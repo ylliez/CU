9e460200be351ffb25c22534a3e5b77f8f1716b9
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-12 15:20:48 -0400

    Started drawing experiments

---
+++ sandboxes/drawing-experiments/js/script.js
@@ -0,0 +1,36 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(500,500);
+  background(127, 255, 127);
+  line(0,0,500,500);
+  rect(200,200,100,100);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
