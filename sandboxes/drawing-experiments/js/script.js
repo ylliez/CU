1ac0fb0261c8da484b14d14178915471a345bba3
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-12 15:44:09 -0400

    drawing - concatenated greyscale

--- sandboxes/drawing-experiments/js/script.js
+++ sandboxes/drawing-experiments/js/script.js
@@ -22,15 +22,16 @@ Description of setup
 */
 function setup() {
   createCanvas(500,500);
-  background(255, 127, 127);
-  fill(255,0,0);
-  rect(0,0,250,250);
-  fill(254,0,0);
-  rect(250,0,250,250);
-  fill(235,0,0);
-  rect(0,250,250,250);
-  fill(200,0,0);
-  rect(250,250,250,250);
+  background(255);
+  rectMode(CENTER);
+  fill(200);
+  rect(250,250,400,400);
+  fill(175);
+  rect(250,250,300,300);
+  fill(150);
+  rect(250,250,200,200);
+  fill(125);
+  rect(250,250,100,100);
 }
 
 
