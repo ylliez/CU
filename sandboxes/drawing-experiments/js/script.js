654013d3f587af8ab22b2f3ceb533ca8b4ae5787
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-12 15:49:15 -0400

    drawing - concatenated alpha

--- sandboxes/drawing-experiments/js/script.js
+++ sandboxes/drawing-experiments/js/script.js
@@ -24,12 +24,19 @@ function setup() {
   createCanvas(500,500);
   background(255);
   rectMode(CENTER);
+  noStroke();
   fill(200);
   rect(250,250,400,400);
+  fill(255,0,0,127);
+  rect(250,250,350,350);
   fill(175);
   rect(250,250,300,300);
+  fill(0,255,0,127);
+  rect(250,250,250,250);
   fill(150);
   rect(250,250,200,200);
+  fill(0,0,255,127);
+  rect(250,250,150,150);
   fill(125);
   rect(250,250,100,100);
 }
