dce10bc49424b145e7f539c6c1b10867eece75fe
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-13 16:57:05 -0400

    curve v1

--- sandboxes/sdbx02_curves/js/script.js
+++ sandboxes/sdbx02_curves/js/script.js
@@ -21,7 +21,21 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(400, 400);
+  background(0);
+  stroke(255);
+  noFill();
+  bezier(0,0,width/3,height/2,2*width/3,height/2,width,0);
+  bezier(0,0,width/3,height/1.9,2*width/3,height/1.9,width,0);
+  bezier(0,0,width/3,height/1.8,2*width/3,height/1.8,width,0);
+  bezier(0,0,width/3,height/1.7,2*width/3,height/1.7,width,0);
+  bezier(0,0,width/3,height/1.6,2*width/3,height/1.6,width,0);
+  bezier(0,0,width/3,height/1.5,2*width/3,height/1.5,width,0);
+  bezier(0,0,width/3,height/1.4,2*width/3,height/1.4,width,0);
+  bezier(0,0,width/3,height/1.3,2*width/3,height/1.3,width,0);
+  bezier(0,0,width/3,height/1.2,2*width/3,height/1.2,width,0);
+  bezier(0,0,width/3,height/1.1,2*width/3,height/1.1,width,0);
+  bezier(0,0,width/3,height,2*width/3,height,width,0);
 }
 
 
@@ -30,4 +44,3 @@ Description of draw()
 */
 function draw() {
 
-}
