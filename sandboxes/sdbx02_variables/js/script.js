935561d01c5f17c0708425ccfafd8ed1b93ea462
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-16 23:21:00 -0400

    changed sandbox variables style

--- sandboxes/sdbx02_variables/js/script.js
+++ sandboxes/sdbx02_variables/js/script.js
@@ -13,7 +13,9 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(500,500);
+//  createCanvas(500,500);
+//  createCanvas(640,640);
+  createCanvas(windowWidth,windowHeight);
 }
 
 
@@ -21,5 +23,13 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+  // bckgd
+  background(255,0,0);
+//  background(mouseX,mouseY,0);
+  //draw a square in the centre of canvas
+  rectMode(CENTER);
+//  rect(250,250,100,100);
+//  rect(mouseX,mouseY,100,100);
+//  rect(250,250,mouseX,mouseY)
+  rect(width/2,height/2,100,100);
 }
