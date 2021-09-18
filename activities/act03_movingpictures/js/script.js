62c12692368bc62dab0a2d6a88ee6d0448aba425
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-18 12:48:02 -0400

    Update script.js

--- activities/act03_movingpictures/js/script.js
+++ activities/act03_movingpictures/js/script.js
@@ -19,18 +19,18 @@ let circle1 = {
   x: 0,
   y: 250,
   size: 100,
-  fill: 63,
+  fill: 255,
   alpha: 200,
   speed: 1,
   growth: 1
 }
 
 let circle2 = {
-  x:500,
+  x: 500,
   y: 250,
   size: 90,
-  fill: 127,
-  alpha: 100,
+  fill: 255,
+  alpha: 200,
   speed: -1,
   proportion: 0.9
 }
@@ -45,17 +45,17 @@ function setup() {
 
 function draw() {
   // background
+  background(bg.r,bg.g,bg.b);
   bg.r = map(circle1.size,100,width,0,255);
-  background(bg.r,bg.b,bg.g);
   // circle 1
-  fill(circle1.fill,circle1.fill,circle1.fill,circle1.alpha);
+  fill(circle1.fill,circle1.alpha);
   ellipse(circle1.x,circle1.y,circle1.size);
   circle1.x += circle1.speed;
   circle1.x = constrain(circle1.x,0,width/2);
   circle1.size += circle1.growth;
   circle1.size = constrain(circle1.size,0,width);
   // circle 2
-  fill(circle2.fill,circle2.fill,circle2.fill,circle2.alpha);
+  fill(circle2.fill,circle2.alpha);
   ellipse(circle2.x,circle2.y,circle2.size);
   circle2.x += circle2.speed;
   circle2.x = constrain(circle2.x,width/2,width);
