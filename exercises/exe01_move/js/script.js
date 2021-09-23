e9ef9e4fa2e4c7e9d6ca7ff22b9601890778a45a
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-23 18:46:53 -0400

    started exercise 01 i like to move it

---
+++ exercises/exe01_move/js/script.js
@@ -0,0 +1,35 @@
+/**
+Exercise 01 - I like to move it
+illiez
+
+A program w/ min 3 shapes that move, change size and color (experimentation).
+Feel free to begin with the code you built in activity 3.
+Use map() & constrain() as well as mouseX and mouseY
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  canvas(windowWidth,windowHeight);
+  background(0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
