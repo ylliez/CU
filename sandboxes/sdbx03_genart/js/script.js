5dd1ce71a4f0751bce1c4d478838a4fe2abffa47
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-23 14:27:57 -0400

    genart first attempt

--- sandboxes/sdbx03_genart/js/script.js
+++ sandboxes/sdbx03_genart/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Generative Art Sandbox
+illie
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -9,11 +9,14 @@ author, and this description to match your project!
 "use strict";
 
 
-/**
-Description of preload
-*/
-function preload() {
-
+let rectangle = {
+  x: 0,
+  y: 0,
+  size: 5,
+  vx: 0,
+  vy: 0,
+  shade: 255,
+  sizeAngle: 0
 }
 
 
@@ -21,7 +24,13 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(500,500);
+  background(0);
+  //rectangle.vx = random(5);
+  //rectangle.vy = random(5);
+  //rectangle.growth = random(5);
+  rectangle.vx = 5;
+  rectangle.vy = 5;
 }
 
 
@@ -29,5 +38,21 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  rectangle.x += rectangle.vx;
+  rectangle.y += rectangle.vy;
+  rectangle.sizeAngle += 0.1;
+  rectangle.size = map(sin(rectangle.sizeAngle),-1,1,10,200);
+  rectangle.size += random(-2,2);
+  rectangle.shade = map(sin(rectangle.sizeAngle),-1,1,10,200);
+
+  rectMode(CENTER);
+  noFill();
+
+  /*let r = map(mouseX,0,width,0,255);
+  let g = map(mouseY,0,height,0,255);
+  stroke(r,g,255);*/
+  stroke(rectangle.shade);
+  rect(rectangle.x,rectangle.y,rectangle.size,rectangle.size);
+
 
 }
