d382bd86ff1703eb2dbfe27083e3f33f9a61aede
ylliez <90415272+ylliez@users.noreply.github.com>
2021-09-30 22:12:14 -0400

    completed booleans

--- sandboxes/sdbx05_conditionals/js/script.js
+++ sandboxes/sdbx05_conditionals/js/script.js
@@ -8,45 +8,28 @@ author, and this description to match your project!
 
 "use strict";
 
-let backgroundShade = 0;
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  speed: 1,
-  fill: 255
-};
+let displayCircle = false;
 
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-  background(backgroundShade);
-  backgroundShade += 0.5;
-  if (backgroundShade == 255) {
-    backgroundShade = 0;
+  // if (mouseIsPressed) { background(255); }
+  // else { background(0); }
+
+  // if (keyIsPressed) { background(255);}
+  // else { background(0); }
+
+  if (mouseIsPressed) {
+    background(255);
+    displayCircle = true;
+  }
+  else {
+    background(0);
   }
 
-  circle.x += circle.speed;
-
-  if (circle.x > width) {
-    circle.speed = -circle.speed; // Change the speed from positive to negative!
+  if (displayCircle) {
+    ellipse(width/2,height/2,100,100); 
   }
-  if (circle.x < 0) {
-    circle.speed = -circle.speed; // Change the speed from positive to negative!
-  }
-
-   fill(circle.fill);
-  // if (mouseX < width/3) { fill(255,0,0); }
-  // else if (mouseX > 2*width/3) { fill(0,0,255); }
-  // else { fill(0,255,0); }
-
-  // if (mouseX > width/3 && mouseX < 2*width/3) { fill(255,0,0); }
-  // else { fill(0,0,0); }
-
-  if (mouseX < width/3 || mouseX > 2*width/3) { fill(255,0,0); }
-  else { fill(0,0,0); }
-
-  ellipse(circle.x, circle.y, circle.size);
 }
