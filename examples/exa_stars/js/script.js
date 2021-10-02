6271ea70ae8ca88a95e1a3d4849a936588c40d93
ylliez <90415272+ylliez@users.noreply.github.com>
2021-10-02 10:18:40 -0400

    created example static

--- examples/exa_stars/js/script.js
+++ examples/exa_stars/js/script.js
@@ -1,33 +1,15 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+let qtyStars = 100;
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(500,500);
+  stroke(255);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
-
+  background(0);
+  for (let i = 0; i < qtyStars; i ++) {
+    point(random(500), random(500));
+  }
 }
