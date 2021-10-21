8d09745e1101759eafdc9517c07f48e542e0ecf5
ylliez <90415272+ylliez@users.noreply.github.com>
2021-10-21 14:40:40 -0400

    class_1021 added array

--- classes/class_1021/js/script.js
+++ classes/class_1021/js/script.js
@@ -1,72 +1,57 @@
 "use strict";
 
-let balloon = undefined;
+let balloons = [];
+let numBalloons = 5;
 
 function setup() {
   createCanvas(500,500);
-  balloon = {
-    x: random(0,width),
-    y: random(0,height),
-    size: 75,
-    fill: color(210, 0, 0),
-    highlightedFill: color(255, 0, 0),
-    normalFill: color(190, 0, 0),
-    popped: false
-  };
+
+  for (let i = 0; i < numBalloons; i++) {
+    let balloon = {
+      x: random(0,width),
+      y: random(0,height),
+      size: 75,
+      fill: color(210, 0, 0),
+      highlightedFill: color(255, 0, 0),
+      normalFill: color(190, 0, 0),
+      popped: false
+    };
+    balloons.push(balloon); //push equivalent to add
+  }
 }
 
 function draw() {
   background(0);
 
-// check for mouseover
-  if (mouseIsInsideBalloon()) { balloon.fill = balloon.highlightedFill; }
-  else { balloon.fill = balloon.normalFill; }
+  for (let i = 0; i < balloons.length; i++) { //rather than numBalloons
+    // rename current array iteration
+    let balloon = balloons[i];
 
-// display the balloon
-  if(!balloon.popped) { // equiv to if(popped = false), so reversed if popped, don't run code, here display
-    push();
-    noStroke();
-    fill(balloon.fill);
-    ellipse(balloon.x,balloon.y,balloon.size);
-    pop();
+    // check for mouseover
+    if (mouseIsInsideBalloon(balloon)) { balloon.fill = balloon.highlightedFill; }
+    else { balloon.fill = balloon.normalFill; }
+
+    // display the balloon
+    if(!balloon.popped) { // equiv to if(popped = false), so reversed if popped, don't run code, here display
+      push();
+      noStroke();
+      fill(balloon.fill);
+      ellipse(balloon.x,balloon.y,balloon.size);
+      pop();
+    }
   }
 }
 
 // instead of checking if within radius twice (hover and press), have a checking function to call
-function mouseIsInsideBalloon() {
+function mouseIsInsideBalloon(balloon) {
   let d = dist(mouseX, mouseY, balloon.x, balloon.y);
   if (d < balloon.size / 2) { return true; }
   else { return false; }
 }
 
 function mousePressed() {
-  if (mouseIsInsideBalloon() && mousePressed) { balloon.popped = true; }
+  for (let i = 0; i < balloons.length; i++) {
+    let balloon = balloons[i];
+    if (mouseIsInsideBalloon(balloon) && mousePressed) { balloon.popped = true; }
+  }
 }
-
-//
-// function draw() {
-//   if (state === 'start') {
-//     start();
-//   }
-//   else if (state === 'simulation') {
-//     simulation();
-//   }
-// }
-//
-// function start() {
-//   background(0);
-//
-//    if(startInstructionVisible) {
-//     push();
-//     textAlign(CENTER,CENTER);
-//     fill(255);
-//     text('CLICK TO BEGIN', width/2, height/2);
-//     pop();
-//   }
-// }
-//
-// function simulation() {
-//   background(bgImage);
-// }
-//
-//
