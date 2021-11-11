f63df31db3fb46c8207022a0318382be2d48cc66
ylliez <90415272+ylliez@users.noreply.github.com>
2021-11-11 12:40:44 -0500

    create ex1111

--- CART211_ex/CART211_ex1111.js
+++ CART211_ex/CART211_ex1111.js
@@ -1,49 +1,42 @@
-let lastTit = "TEST SWAP";
-let lastImg = "../CART211_media/vhugo.jpg";
-const audio1 = new Audio("../CART211_media/boing.wav"); //constructor, creates *new* object
-const audio2 = new Audio("../CART211_media/derek_cloud9.mp3");
-let imgX = 0;
-let imgY = 0;
+let dragX = 0;
+let dragY = 0;
+// let dragX = window.localStorage.getItem("storedX"); //retrieving something from local storage
+// let dragY = window.localStorage.getItem("storedY");
+let dragOffsetH = 0; //offset to center/position cursor on image
+let dragOffsetV = 0;
+let dragging = false;
 
-//setInterval(moveImg,20); // function is the parameter of call, so no () + # of ms
-document.addEventListener("keydown", keyboard); //keypress once on keydown; keydown continuously while keydown
+// //if (typeof(storage) != "undefined") { //if storage is not of the type "undefined"
+  dragX = window.localStorage.getItem("storedX"); //retrieving something from local storage
+  dragY = window.localStorage.getItem("storedY");
+// //}
 
-function clickTitle() {
-  audio1.play();
-  let swapTit = lastTit;
-  lastTit = document.getElementById('dynamicHeader').innerHTML;
-  document.getElementById('dynamicHeader').innerHTML = swapTit;
+function onLoad() {
+  // dragX = window.localStorage.getItem("storedX"); //retrieving something from local storage
+  // dragY = window.localStorage.getItem("storedY");
+  document.getElementById('dragImg').style.left = dragX + "px";
+  document.getElementById('dragImg').style.top = dragY + "px";
 }
 
-function clickImg() {
-  audio1.play();
-  let swapImg = lastImg;
-  lastImg = document.getElementById('dynamicImg').src; //document refers to html doc, with getEl.. an object
-  document.getElementById('dynamicImg').src = swapImg;
-}
-
-function keyboard(event) {
-  console.log("log test");
-  if (event.keyCode == 37) { imgX -= 3; } //arrow left
-  else if (event.keyCode == 39) { imgX += 3; } //arrow right
-  else if (event.keyCode == 38) { imgY -= 3; } //arrow up
-  else if (event.keyCode == 40) { imgY += 3; } //arrow down
-  document.getElementById('dynamicImg').style.left = imgX+"px";
-  document.getElementById('dynamicImg').style.top = imgY+"px";
-
-  if (event.keyCode == 65) { //a
-    let array = document.getElementsByClassName("specialp"); //refers to array
-    for (let i = 0; i < array.length; i++) {
-      array[i].style.fontSize = "40px";
-      array[i].style.color = "#CCAABB";
-    }
-   }
-
-  if (imgX <= -100 && imgY <= -50) {
-    window.alert("you win!"); //not document, browser iaoi
-    document.getElementById('dynamicImg').style.left = "0px";
-    document.getElementById('dynamicImg').style.top = "0px";
-    imgX = 0;
-    imgY = 0;
+function mouseMove()
+{
+  if (dragging)
+  {
+  dragX = event.pageX - dragOffsetH; // event.pageX refers the x position of mouse on the page
+  document.getElementById('dragImg').style.left = dragX + "px";
+  dragY = event.pageY - dragOffsetV;
+  document.getElementById('dragImg').style.top = dragY + "px";
   }
+}
+
+function mouseDown() {
+  dragOffsetH =  100;//event.pageX - document.getElementById('dragImg').style.left; //attempt at making it
+  dragOffsetV =  150;//event.pageY - document.getElementById('dragImg').style.top; //relative to cursor pos
+  dragging = true;
+}
+
+function mouseUp() {
+  dragging = false;
+  window.localStorage.setItem("storedX",dragX); //saving something to local storage (name,value)
+  window.localStorage.setItem("storedY",dragY);
 }
