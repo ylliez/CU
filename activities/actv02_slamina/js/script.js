12b357e8a11597c93ea5fef6fa43228964bb86aa
ylliez <90415272+ylliez@users.noreply.github.com>
2022-01-13 13:01:10 -0500

    actv02_slamina started
    created actv01_slamina

---
+++ activities/actv02_slamina/js/script.js
@@ -0,0 +1,23 @@
+/**
+Slamina
+illiez
+
+The program will speak the name of a common animal backwards and the user will have to say (with their voice) what they think it is in the form “I think it is x.” If they get it right, their guess will be displayed in green, if they get it wrong, their guess will be displayed in red.
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {}
+
+/**
+Description of draw()
+*/
+function draw() {}
