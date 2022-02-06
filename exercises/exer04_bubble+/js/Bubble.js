0602eae76b00475e417147457c9010150d13a839
ylliez <90415272+ylliez@users.noreply.github.com>
2022-02-06 18:28:07 -0500

    exer04_bubble+ started and organised
    Ex: created folder for Exercise 04 - Bubble Popper + and located in GitHub Pages

---
+++ exercises/exer04_bubble+/js/Bubble.js
@@ -0,0 +1,42 @@
+class Bubble {
+
+  constructor() {
+    this.x = random(width);
+    this.y = height;
+    this.size = 15;
+    this.vx = 0;
+    this.vy = -2;
+  }
+
+  update() {
+    this.display();
+    this.move();
+    this.wrap();
+  }
+
+  display() {
+    push();
+    fill(50, 50, 255);
+    noStroke();
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  wrap() {
+    if (this.y < 0) {
+      this.y = height;
+    }
+  }
+
+  reset() {
+    this.y = 0;
+    this.vy += 1;
+    this.size += 5;
+  }
+
+}
