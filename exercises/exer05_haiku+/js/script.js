d4541a9e73438d93449f3d63866755d96d48ffb3
ylliez <90415272+ylliez@users.noreply.github.com>
2022-02-24 00:10:47 -0500

    exer05_haiku+ page setup, buttons,  DOM hovering
    Ex: organized the page HTML/CSS, added buttons to generate entirely new poem and to print the page, and added hover-over size change to the lines

--- exercises/exer05_haiku+/js/script.js
+++ exercises/exer05_haiku+/js/script.js
@@ -2,12 +2,14 @@
 
 let fiveSyllableLines = [];
 let sevenSyllableLines = [];
-let fiveSyllableLinesObj, sevenSyllableLinesObj, fiveSyllableLinesKey, sevenSyllableLinesKey;
+let fiveSyllableLinesObj, sevenSyllableLinesObj;
 let lineP = [];
 let sentiment, prediction;
 let text = [];
 let predictionMood = [];
 let predictionColor;
+let buttons = [];
+let doOnce = true;
 
 function preload() {
   fiveSyllableLinesObj = loadJSON("assets/data/fiveSyllableLines.json");
@@ -16,19 +18,44 @@ function preload() {
 
 function setup() {
   // create Sentiment method
-  sentiment = ml5.sentiment('movieReviews', getHaikuMood);
+  sentiment = ml5.sentiment('movieReviews', modelLoaded);
   // get DOM elements of lines, add click function
   for (var i = 0; i < 3; i++) {
     lineP[i] = document.getElementById(`line-${i+1}`);
+    lineP[i].addEventListener(`mouseenter`, () => { lineEntered(event); });
+    lineP[i].addEventListener(`mouseleave`, () => { lineExited(event); });
     lineP[i].addEventListener(`click`, () => { lineClicked(event); });
   }
   // parse JSON file objects into arrays
-  fiveSyllableLinesKey = Object.keys(fiveSyllableLinesObj);
+  let fiveSyllableLinesKey = Object.keys(fiveSyllableLinesObj);
   for (let i = 0; i < fiveSyllableLinesKey.length; i++) { fiveSyllableLines[i] = fiveSyllableLinesObj[fiveSyllableLinesKey[i]]; }
-  sevenSyllableLinesKey = Object.keys(sevenSyllableLinesObj);
+  let sevenSyllableLinesKey = Object.keys(sevenSyllableLinesObj);
   for (let i = 0; i < sevenSyllableLinesKey.length; i++) { sevenSyllableLines[i] = sevenSyllableLinesObj[sevenSyllableLinesKey[i]]; }
+
+}
+
+function modelLoaded() {
   // load first set of haiku lines
+  generateNewHaiku();
+  buttons = document.getElementsByTagName('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].style.display = `block`;
+  }
+}
+
+function buttNewClicked() {
+  if (doOnce) {
+    alert(`Esteemed Poet,
+You may also change the haiku line by line
+Click where your heart deems good...`);
+    doOnce = false
+  }
+  generateNewHaiku();
+}
+
+function generateNewHaiku() {
   for (var i = 0; i < 3; i++) { setNewLine(lineP[i]); }
+  getHaikuMood();
 }
 
 function setNewLine(element) {
@@ -42,18 +69,25 @@ function getHaikuMood() {
     prediction = sentiment.predict(text[i]);
     predictionMood[i] = (floor(map(prediction.score, 0, 1, 0, 16)).toString(16));
   }
-  predictionColor = `#${predictionMood[0]}${predictionMood[1]}${predictionMood[2]}`;
+  colorBackground();
+}
+
+function colorBackground() {
+  let predictionColor = `#${predictionMood[0]}${predictionMood[1]}${predictionMood[2]}`;
+  // DEBUG - print color hex code
   // console.log(predictionColor);
   document.body.style[`background-color`] = predictionColor;
 }
 
-function lineClicked(event) {
-  fadeOut(event.target, 1);
-}
+function lineEntered(event) { event.target.style.fontSize = `35px`; }
+
+function lineExited(event) { event.target.style.fontSize = `30px`; }
+
+function lineClicked(event) { fadeOut(event.target, 1); }
 
 function fadeOut(element, opacity) {
   opacity -= 0.01;
-  element.style.opacity = opacity;
+  element.style[`opacity`] = opacity;
   if (opacity > 0) {
     requestAnimationFrame(() => { fadeOut(element, opacity); } );
   }
@@ -71,8 +105,4 @@ function fadeIn(element, opacity) {
     requestAnimationFrame(() => { fadeIn(element, opacity); } );
   }
   else {  }
-}
-
-function draw() {
-
 }
