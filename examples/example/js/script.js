39c977dccdc6c817a939452d81d831995ebbb9b2
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-02 11:58:55 -0500

    Update script.js

--- examples/example/js/script.js
+++ examples/example/js/script.js
@@ -97,18 +97,20 @@ function dodo() {
   for (let i = 0; i < words.length; i++) {
     for (let j = 0; j < letters.length; j++) {
       for (var k = 0; k < letters.length; k++) {
-        let sol = `s${letters[j]}a${letters[k]}e`;
+        let sol = `sant${letters[j]}`;
+        // let sol = `sant${letters[j]}${letters[k]}`;
         // console.log(sol);
-        let non = [`t`,`r`,`c`,`o`,`l`,`i`,`n`,`d`,`u`,`m`,`p`,`y`];
-        let notest = true;
-
-        if (sol === words[i] && () => {
-          for (let l = 0; l < non.length; l++) {
-            if(non[l] === letters[i]) { return false };
-            if(non[l] === letters[j]) { return false };
-            if(non[l] === letters[k]) { return false };
-          }
-        }) {
+        // let non = [`t`,`r`,`c`,`o`,`l`,`i`,`n`,`d`,`u`,`m`,`p`,`y`];
+        // let notest = true;
+        //
+        if (sol === words[i]) {
+        // if (sol === words[i] && () => {
+        //   for (let l = 0; l < non.length; l++) {
+        //     if(non[l] === letters[i]) { return false };
+        //     if(non[l] === letters[j]) { return false };
+        //     if(non[l] === letters[k]) { return false };
+        //   }
+        // }) {
           console.log(sol);
         }
       }
