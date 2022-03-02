7075200291e5860e85637afde6c9f152ea4a01d9
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-02 18:06:42 -0500

    Update script.js

--- exercises/exer06_redact+/js/script.js
+++ exercises/exer06_redact+/js/script.js
@@ -9,16 +9,31 @@ $(`.top-secret`).on(`click`,redact);
 
 function revelation() {
   $(`.redacted`).each(attemptReveal);
+  // $(`.redacted`).each(attemptReveal, checkEnd);
 }
 
 function attemptReveal() {
   if (Math.random() < REVEAL_PROB) {
     $(this).removeClass(`redacted`);
     $(this).addClass(`revealed`);
+    checkEnd();
   }
 }
 
 function redact() {
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
+}
+
+function checkEnd() {
+  if (!$(`.redacted`).length) {
+    gameOver();
+    // requestAnimationFrame(gameOver);
+  }
+}
+
+function gameOver() {
+  // $(`#secret-document`).hide();
+  // $(`.top-secret`).css(`cursor`, `none`);
+  alert(`You lose!`);
 }
