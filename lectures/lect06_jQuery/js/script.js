ccb3c05ad679787f284438b61af7bddb607d8b70
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-02 12:48:38 -0500

    Update script.js

--- lectures/lect06_jQuery/js/script.js
+++ lectures/lect06_jQuery/js/script.js
@@ -28,9 +28,23 @@
 // $mainHeading.css(`font-family`, `Helvetica, sans-serif`);
 // $mainHeading.css(`background-color`, `#000000`);
 
-$(`#main-heading`).css({
-  "color": `#339966`,
-  "font-size": `5rem`,
-  "font-family": `Helvetica, sans-serif`,
-  "background-color": `#000000`
-});
+// $(`#main-heading`).css({
+//   "color": `#339966`,
+//   "font-size": `5rem`,
+//   "font-family": `Helvetica, sans-serif`,
+//   "background-color": `#000000`
+// });
+
+// $(`#example-span`).text(`a Spaniel`);
+
+// // Get the current text in the span
+// let spanText = $(`#example-span`).text();
+// // Reverse it
+// let reverseSpanText = spanText.split(``).reverse().join(``);
+// // Set the span's text to the reversed version
+// $(`#example-span`).text(reverseSpanText);
+
+// // Get the HTML content of the span
+// let spanHTML = $(`#example-span`).html();
+// // Set the HTML content of the span as the original content wrapped in a <strong> tag
+// $(`#example-span`).html(`<strong>${spanHTML}</strong>`);
