354f3606f851ff2df675386781e5cdded62f456f
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-02 12:29:27 -0500

    Update script.js

--- lectures/lect06_jQuery/js/script.js
+++ lectures/lect06_jQuery/js/script.js
@@ -1,3 +1,6 @@
 "use strict";
 
-// Code goes here
+// Get the <h1> element by its id and store it in a variable
+let $mainHeading = $(`#main-heading`);
+// Set its color property using the .css() method
+$mainHeading.css(`color`, `#339966`);
