544a03afbbc518e4f489928ca2834b76c776c084
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-08 12:29:32 -0500

    Update script.js

--- projects/proj02_anything/proj02_telomatic/js/script.js
+++ projects/proj02_anything/proj02_telomatic/js/script.js
@@ -123,7 +123,7 @@ function keyPressed() {
   // 'f' key toggled fullscreen
   if (key === 70) {    }
   // 'c' key toggled connection
-  if (key === 67) {
+  if (keyCode === 67) {
     if (!teloBLE.isConnected()) { connectToBLE(); }
     else { disconnectFromBLE(); }
   }
