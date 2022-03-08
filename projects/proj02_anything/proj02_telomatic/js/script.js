53b32058128dd9bbbf1b1b5f2322ea78b18b8644
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-08 16:21:26 -0500

    Update script.js

--- projects/proj02_anything/proj02_telomatic/js/script.js
+++ projects/proj02_anything/proj02_telomatic/js/script.js
@@ -106,19 +106,19 @@ function drawIndexTip() {
 
 function writeToBLE() {
   if (teloBLE.isConnected() && teloCharacteristic) {
-    // teloIntensity = 255 - floor(hand.index.y / height * 255);
-    if (predictions.length > 0 && hand.index.y > 50) { teloIntensity = 255 - floor(hand.index.y / height * 255); }
-    else { teloIntensity = 0 }
+    teloIntensity = 255 - floor(hand.index.y / height * 255);
+    // if (predictions.length > 0 && hand.index.y > 50) { teloIntensity = 255 - floor(hand.index.y / height * 255); }
+    // else { teloIntensity = 0 }
     teloBLE.write(teloCharacteristic, teloIntensity);
-    // push();
-    // textSize(30);
-    // textStyle(BOLD);
-    // textAlign(CENTER, CENTER);
-    // fill(255);
-    // text(hand.index.y, width / 4 * 3, height / 4);
-    // text(floor(hand.index.y / height * 255), width / 4 * 3, height / 2);
-    // text(teloIntensity, width / 4 * 3, height / 4*3);
-    // pop();
+    push();
+    textSize(30);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text(hand.index.y, width / 4 * 3, height / 4);
+    text(floor(hand.index.y / height * 255), width / 4 * 3, height / 2);
+    text(teloIntensity, width / 4 * 3, height / 4*3);
+    pop();
   }
 }
 
