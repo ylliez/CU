7efe92cf03d03d1b3f6ca09ebf5623ffaca6fe45
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-10 15:20:08 -0500

    exer06_redact added intro instructions

--- exercises/exer06_redact+/js/script.js
+++ exercises/exer06_redact+/js/script.js
@@ -1,15 +1,28 @@
+// TO ADD
+// Start page
+// selectable redaction?
+// ^\w*\s\d+:\d+\s
+/* image dims: 1792*1008 */
+
 "use strict";
+let state = `title`;
 // holders for grammar JSON & object
-let json, grammar;
+let grammarJSON, grammar;
 const NUM_COMMANDS = 10;
 const REVEAL_PROB = 0.01;
 const ATTEMPT_FREQ = 50;
 // holders for sound effects and info
 let sfx = [];
 const NUM_SOUNDS = 8;
+// holder for diagetic text elements
+let textIntro;
+const END_TEXT = `
+And all the people saw the thunderings, and the lightnings, and the noise of the trumpet, and the mountain smoking.
+The commandments have been set in stone. Would you like a printed copy to safeguard your soul?`;
 
 function preload() {
-  json = loadJSON('assets/data/grammar.json');
+  grammarJSON = loadJSON('assets/data/grammar.json');
+  textIntro = loadJSON('assets/data/instructions.json');
   for (let i = 0; i < NUM_SOUNDS; i++) {
     sfx[i] = loadSound(`assets/audio/sfx_holy_${i}.wav`);
   }
@@ -17,9 +30,19 @@ function preload() {
 
 function setup() {
   // set RiTa grammar rules
-  grammar = RiTa.grammar(json);
+  grammar = RiTa.grammar(grammarJSON);
+  // set click start feature
+  $('body').one(`click`, startSim);
+}
+
+function startSim() {
+  $('#intro').empty();
+  // $('#intro').css(`opacity`, `0`);
+  $('#startClk').empty();
   // generate 10 lines following the grammar
   for (let i = 0; i < NUM_COMMANDS; i++) {
+    // let id = `#cmd${i}`;
+    // $(id).innerHTML = grammar.expand();
     document.getElementById(`cmd${i}`).innerHTML = grammar.expand();
   }
   // start timer for intervallic line revelation
@@ -28,20 +51,22 @@ function setup() {
   $(`.apocrypha`).on(`click`, obfuscate);
 }
 
-function draw() {
-}
+//
+// function draw() {
+//   switch (state) {
+//     case `title`: title(); break;
+//     case `sim`: sim(); break;
+//   }
+// }
 
-function revelation() {
-  // $(`.esoteric`).each(attemptReveal);
-  $(`.esoteric`).each(attemptReveal, checkEnd);
-}
+function revelation() { $(`.esoteric`).each(attemptReveal); }
 
 function attemptReveal() {
   if (Math.random() < REVEAL_PROB) {
     random(sfx).play();
+    $(this).animate( { "opacity": 1 }, 1000, checkEnd);
     $(this).removeClass(`esoteric`);
     $(this).addClass(`revealed`);
-    $(this).animate( { "opacity": 1 }, 1000, checkEnd);
   }
 }
 
@@ -55,23 +80,6 @@ function obfuscate() {
 
 function checkEnd() {
   if (!$(`.esoteric`).length) {
-    gameOver();
-    // requestAnimationFrame(gameOver);
+    if(confirm(END_TEXT)) { window.print(); }
   }
 }
-
-function gameOver() {
-  // $(`#secret-document`).obfuscate();
-  // $(`.apocrypha`).css(`cursor`, `none`);
-  if(confirm(`And all the people saw the thunderings, and the lightnings, and the noise of the trumpet, and the mountain smoking.
-The commandments have been set in stone. Would you like a printed copy to safeguard your soul?`)) {
-      window.print();
-    }
-}
-
-/* https://pippinbarr.github.io/cart263/exercises/raving-redactionist-plus-plus.html
-- Use a fading effect to fade the redaction effect in and out (this will likely require .animate())
-- Allow the user to redact by clicking and dragging the mouse cursor over a reveal passage rather than clicking
-- Add an ending of some kind to the experience if the user allows all the secrets to be revealed (“YOU LOST THE COLD WAR!!”)
-- Have a computer voice read out revealed secrets and have the user “lose” if the computer finishes reading before they’ve esoteric it
-*/
