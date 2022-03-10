bc1a84a12360a9863e2ffd903c95679c80a20d0f
ylliez <90415272+ylliez@users.noreply.github.com>
2022-03-10 10:39:42 -0500

    exer06_redact joined reveal/hide feature to commandments

--- exercises/exer06_redact+/js/script.js
+++ exercises/exer06_redact+/js/script.js
@@ -1,58 +1,62 @@
 "use strict";
-let json, grammar, result;
+// holders for grammar JSON & object
+let json, grammar;
+// holder for grammar generated text lines
 let lines = []
 const NUM_COMMANDS = 10;
 const REVEAL_PROB = 0.1;
 const ATTEMPT_FREQ = 1000;
 
-function preload() {
-  json = loadJSON('assets/data/grammar.json');
-}
+function preload() { json = loadJSON('assets/data/grammar.json'); }
 
 function setup() {
+  // set RiTa grammar rules
   grammar = RiTa.grammar(json);
+  // generate 10 lines following the grammar
   for (let i = 0; i < NUM_COMMANDS; i++) {
     lines[i] = grammar.expand();
+    document.getElementById(`cmd${i}`).innerHTML = lines[i];
   }
-
+  // start timer for intervallic line revelation
   setInterval(revelation, ATTEMPT_FREQ);
-
-  $(`.apocrypha`).on(`click`,redact);
+  // set query for click action, hides & changes command
+  $(`.apocrypha`).on(`click`, obfuscate);
 }
 
 function draw() {
   for (let i = 0; i < NUM_COMMANDS; i++) {
-    document.getElementById(`cmd${i}`).innerHTML = lines[i];
+
   }
 }
 
 function revelation() {
-  $(`.hidden`).each(attemptReveal);
-  // $(`.hidden`).each(attemptReveal, checkEnd);
+  $(`.esoteric`).each(attemptReveal);
+  // $(`.esoteric`).each(attemptReveal, checkEnd);
 }
 
 function attemptReveal() {
   if (Math.random() < REVEAL_PROB) {
-    $(this).removeClass(`hidden`);
+    $(this).removeClass(`esoteric`);
     $(this).addClass(`revealed`);
     checkEnd();
   }
 }
 
-function redact() {
+function obfuscate() {
   $(this).removeClass(`revealed`);
-  $(this).addClass(`hidden`);
+  $(this).addClass(`esoteric`);
+  $(this)[0].innerHTML = grammar.expand();
 }
 
 function checkEnd() {
-  if (!$(`.hidden`).length) {
+  if (!$(`.esoteric`).length) {
     gameOver();
     // requestAnimationFrame(gameOver);
   }
 }
 
 function gameOver() {
-  // $(`#secret-document`).hide();
+  // $(`#secret-document`).obfuscate();
   // $(`.apocrypha`).css(`cursor`, `none`);
   alert(`You lose!`);
 }
@@ -63,8 +67,8 @@ function gameOver() {
 - Add audio to the experience (background music? the sound of a boring office? a pen squealing sound when you redact something? a gasp when something is revealed?)
 - Allow the user to redact by clicking and dragging the mouse cursor over a reveal passage rather than clicking
 - Add an ending of some kind to the experience if the user allows all the secrets to be revealed (“YOU LOST THE COLD WAR!!”)
-- Have a computer voice read out revealed secrets and have the user “lose” if the computer finishes reading before they’ve hidden it
+- Have a computer voice read out revealed secrets and have the user “lose” if the computer finishes reading before they’ve esoteric it
 - Make the secrets float off the “page” instead of just being revealed, and if they make it off the edge they’re gone forever - clicking them makes them go back to where they started
 - Generate a text for redaction in code instead of having it already in the HTML (you’ll have to generate the redaction spans as well)
-- Use ml5.js’s text features of a library like RiTa to generate the text being hidden
+- Use ml5.js’s text features of a library like RiTa to generate the text being esoteric
 */
