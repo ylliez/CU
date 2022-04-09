781d06d8fcecf3568a6d3f4eb07b8172e26c94ec
ylliez <90415272+ylliez@users.noreply.github.com>
2022-04-09 11:56:57 -0400

    commented PHP script
    proj02_telomatic: added comments to upload.php script for clarification and posterity

--- projects/proj02_telomatic/upload.php
+++ projects/proj02_telomatic/upload.php
@@ -1,13 +1,16 @@
 <?php
+// set upload directory in same root folder
 $upload_dir = "upload/";
-$img = $_POST['p5CanvasImage'];
+// receive URI from ajax POST using key
+$img = $_POST['canvasImage'];
+// format received URI (replace leader and spaces)
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
+// decode URI
 $data = base64_decode($img);
-$file = $upload_dir . mktime() . ".png";
-// $file = $upload_dir.$imgName.".png";
-//save to file...
+// create file for data and save data to file
+$file = $upload_dir.mktime().".png";
 $success = file_put_contents($file, $data);
+// return file URL
 echo($file);
-// echo($upload_dir . $imgName . ".png");
 ?>
