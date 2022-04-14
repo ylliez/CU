796ca778c5b5139f4080fae544d584c9ccdd3a1c
ylliez <90415272+ylliez@users.noreply.github.com>
2022-04-14 16:16:47 -0400

    clean & comment gallery HTML & PHP
    proj02_telomatic: removed previous back-up attempts & superfluous code, added explicative comments

--- projects/proj02_telomatic/gallery/gallery.php
+++ projects/proj02_telomatic/gallery/gallery.php
@@ -1,23 +1,6 @@
 <?php
 // generic check for GET request
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-
-  // // scan upload directory for files, sort in descending order & echo JSON encoded array back
-  // $images = scandir("upload", 1);
-  // echo(json_encode($images));
-
-  // // scan upload directory for files, sort in descending order, cull unwanted incidental files by array substraction
-  // $images = array_diff(scandir("upload", 1), array('..','.DS_Store'));
-  // // create array for final images to echo
-  // $imagesToSend = [];
-  // // iterate through upload files, cull unwanted incidental files by selective array copying
-  // for($i = 0; $i < count($images); $i++) {
-  //   if(str_starts_with($images[$i],".") == false) {
-  //     // array to array with desired values
-  //     $imagesToSend[] = $images[$i];
-  //   }
-  // }
-
   // scan upload directory for files, sort in descending order, cull unwanted incidental files by array substraction
   $imagesToSend = array_diff(scandir("../upload", 1), array('.','..','.DS_Store'));
   // echo JSON encoded array back
@@ -28,5 +11,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 function str_starts_with ( $haystack, $needle ) {
   return strpos( $haystack , $needle ) === 0;
 }
-
 ?>
