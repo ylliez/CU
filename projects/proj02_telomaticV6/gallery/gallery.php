ea8818d698d081bbbc6db02a02d41637d042d28c
ylliez <90415272+ylliez@users.noreply.github.com>
2022-04-14 17:16:31 -0400

    create pre-cleanup back-up
    roj02_telomatic: made backup of code before cleaning-up/commenting away

---
+++ projects/proj02_telomaticV6/gallery/gallery.php
@@ -0,0 +1,14 @@
+<?php
+// generic check for GET request
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+  // scan upload directory for files, sort in descending order, cull unwanted incidental files by array substraction
+  $imagesToSend = array_diff(scandir("../upload", 1), array('.','..','.DS_Store'));
+  // echo JSON encoded array back
+  echo(json_encode($imagesToSend));
+}
+
+// helper function...
+function str_starts_with ( $haystack, $needle ) {
+  return strpos( $haystack , $needle ) === 0;
+}
+?>
