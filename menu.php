afc67fa6dec4508b4d84b5922d6e7cf0729a1348
illiez <illiez@hotmail.com>
2022-09-12 16:45:32 -0400

    repo structuring

---
+++ menu.php
@@ -0,0 +1,33 @@
+
+    <header>
+    <h1>CART451</h1>
+    <h2><?php echo $title;?></h2>
+    <div class="menu">
+      <ul>
+        <li> <a href="."><i class="fa fa-home"></i></a></li>
+        <li> <a href="resources.html"><i class="fa fa-wrench"></i></a>
+        <ul>
+          <li> <a href="https://moodle.concordia.ca/moodle/course/view.php?id=147835">Moodle</a></li>
+          <li> <a href="https://moodle.concordia.ca/moodle/mod/page/view.php?id=3168348">Syllabus</a></li>
+          <li> <a href="https://moodle.concordia.ca/moodle/mod/page/view.php?id=3171739">Readings</a></li>
+          <li> <a href="https://moodle.concordia.ca/moodle/mod/page/view.php?id=3181876">Presentations</a></li>
+          <li> <a href="https://discord.com/channels/752977423804661832/1016499743318356028">Discord</a></li>
+        </ul>
+      </li>
+        <li> <a href="notes.html"><i class="fa fa-list-ol"></i></a>
+          <ul>
+            <li> <a href="notes.html#0906">Collective Intelligence</a></li>
+            <li> <a href="notes.html#0913">Node.js</a></li>
+            <!-- <li> <a href="notes.html#0920">???</a></li> -->
+          </ul>
+        </li>
+        <!-- <li> <a href="responses.html"><i class="fa fa-download"></i></a> -->
+        <li> <a href="rr.php"><i class="fa fa-question"></i></a>
+          <ul>
+            <li> <a href="rr.php#0913_bio">09/13 - Bio</a></li>
+            <li> <a href="rr.php#0920_???">09/20 - ???</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </header>
