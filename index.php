a964fcd4beb10b380f320636104bad1633ecad39
illiez <illiez@hotmail.com>
2022-09-12 16:47:14 -0400

    repo structuring

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset=“UTF-8”>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CART451 - Home</title>
+  <link rel="icon" type="image/svg" href="assets/icon_network_fw.svg">
+  <link rel="stylesheet" href="style.css">
+  <style> body { background-image: url('assets/bkgd_hella.jpg'); }</style>
+ </head>
+
+  <body>
+    <?php
+      $title = 'Home';
+      include('menu.php');
+    ?>
+
+  </body>
+</html>
---
+++ notes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset=“UTF-8”>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CART451 - Notes</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" type="image/svg" href="assets/icon_network1.svg">
+  </head>
+
+  <body>
+    <h1>HI</h1>
+    <script>
+    <?php
+      $title = 'Notes';
+      include('menu.php');
+      echo "Hello World!";
+    ?>
+    </script>
+  </body>
+</html>
