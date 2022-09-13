650996d30f4c7a4d246fd0f19fd17ebdab7c9a7a
illiez <illiez@hotmail.com>
2022-09-13 00:43:29 -0400

    add response - 0913 - bio

--- responses.php
+++ responses.php
@@ -15,6 +15,10 @@
       .collapsible:after { content: url('assets/telo.ico'); float: right; transform: rotate(90deg); }
       .active:after { content: url('assets/telo.ico'); float: right; transform: rotate(180deg); }
       h4 { font-size: 18px; text-align: left; margin-left: 5%; }
+      #maps { width: 100%; }
+      iframe { position: relative; width: 32vw; height: 20vw; }
+      img { position: relative; margin: 10px; width: 10vw; height: 10vw; }
+      #mallgrabButton { float: right; margin-right: 46vw; margin-bottom: 20px; }
     </style>
   </head>
 
@@ -26,12 +30,48 @@
 
     <button type="button" class="collapsible" id="responses_0913">09/13 - Short Bio</button>
     <div class="content">
-      <h4>PAST</h4>
-      <p></p>
-      <h4>PRESENT</h4>
-      <p></p>
-      <h4>FUTURE</h4>
-      <p></p>
+      <h3>PAST</h3>
+      <div id="maps">
+        <iframe class="map" src="https://maps.google.com/maps?q=biel&t=k&z=13&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe class="map" src="https://maps.google.com/maps?q=detroit&t=k&z=12&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe class="map" src="https://maps.google.com/maps?q=paris&t=k&z=11&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe class="map" src="https://maps.google.com/maps?q=berlin&t=k&z=12&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe class="map" src="https://maps.google.com/maps?q=isone&t=k&z=12&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe class="map" src="https://maps.google.com/maps?q=york&t=k&z=12&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      </div>
+      <div id="logos">
+        <img src="assets/bio/gren.png"></img>
+        <img src="assets/bio/mcgill.svg"></img>
+        <img src="assets/bio/cssdp.png"></img>
+        <img src="assets/bio/mps.png"></img>
+        <img src="assets/bio/d&b.jpeg"></img>
+        <img src="assets/bio/flexxco.svg"></img>
+        <img src="assets/bio/vrplanet.png"></img>
+        <img src="assets/bio/arsenala.jpeg"></img>
+      </div>
+      <h3>PRESENT</h3>
+      <h4>PROGRAM</h4>
+      <p>BFA IMCA/CART</p>
+      <h4>INTERESTS</h4>
+      <p>
+        Tinkering, woodwork, metalwork, textiles<br>
+        Electronic lutherie, sound spatialization, psychoacoustics<br>
+        Interactive visual art, live visual performance<br>
+        Computation arts, network art, text-based ML/AI<br>
+        Performance art, body art, durational art<br>
+        Neuroart, non-normative experiences, neuro/psycho-divergence<br>
+        Linguistics, literature, geography, geopolitics<br>
+      </p>
+      <h4>DORMANT SKILLS</h4>
+      <p>
+        Cherry-stem knot-tying<br>
+        Pyrotechnics<br>
+        Childcare<br>
+        Budgeting<br>
+      </p>
+      <h3>FUTURE</h3>
+      <button id="mallgrabButton" onclick="mallgrabPlay()" type="button" style="float:right">Equanimity</button>
+      <audio id="mallgrab" src="assets/bio/mallgrab.mp3" type="audio/mp3">
     </div>
 
     <!-- <button type="button" class="collapsible" id="responses_0920">09/20 - ???</button>
@@ -58,15 +98,11 @@
     }
     if(self.location.hash=="#responses_0913") { document.getElementById("responses_0913").click(); history.pushState("", document.title, window.location.pathname + window.location.search); }
     if(self.location.hash=="#responses_0920") { document.getElementById("responses_0920").click(); history.pushState("", document.title, window.location.pathname + window.location.search); }
+    
+    var mallgrab = document.getElementById("mallgrab")
+    function mallgrabPlay() { mallgrab.play(); }
     </script>
 
   </body>
 
   </html>
-
-
-<!-- Rather: I would like each one of you to provide a short bio. Recall where to put this.... 
-- your github repo for the cart 451 class - which I will require you to send me the link)
-- inside should be an html file called weekly responses and this is where you append all the responses.
-So: the bio - need not be only/just text - it can be an image, animation, audio  ....  it could also be rather abstract i.e. you discuss something, an experience, a place ... that you feels represents you.
-You choose how you want to be interpreted. -->
